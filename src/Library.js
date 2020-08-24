196c151ae0a72f95d3ff3ce9ae7cd534b71c8333
Sarah Osgood <sarahjs117@gmail.com>
2020-08-23 19:54:04 -0500

    Attempted to refactor without success

--- src/Library.js
+++ src/Library.js
@@ -25,22 +25,22 @@ function checkoutBook(libraryLocation, bookTitle) {
   for (var i = 0; i < libraryLocation.shelves.fantasy.length; i++) {
     if (bookTitle == libraryLocation.shelves.fantasy[i].title) {
         libraryLocation.shelves.fantasy.pop();
-        return `You have now checked out ${bookTitle} from the Denver Public Library`;
+        return `You have now checked out ${bookTitle} from the ${libraryLocation.name}`;
     }
   }
   for (var i = 0; i < libraryLocation.shelves.fiction.length; i++) {
     if (bookTitle == libraryLocation.shelves.fiction[i].title) {
         libraryLocation.shelves.fiction.pop();
-        return `You have now checked out ${bookTitle} from the Denver Public Library`;
+        return `You have now checked out ${bookTitle} from the ${libraryLocation.name}`;
     }
   }
   for (var i = 0; i < libraryLocation.shelves.nonFiction.length; i++) {
     if (bookTitle == libraryLocation.shelves.nonFiction[i].title) {
         libraryLocation.shelves.nonFiction.pop();
-        return `You have now checked out ${bookTitle} from the Denver Public Library`;
+        return `You have now checked out ${bookTitle} from the ${libraryLocation.name}`;
     }
   }
-  return "Sorry, there are currently no copies of The Fifth Season available at the Denver Public Library";
+  return `Sorry, there are currently no copies of ${bookTitle} available at the ${libraryLocation.name}`;
 }
 
 
@@ -49,10 +49,3 @@ module.exports = {
   addBook: addBook,
   checkoutBook: checkoutBook
 };
-
-// check on whether it is better to create a variable and
-// return the variable or to just return the object.
-
-// come back to checkoutBook and refactor to make simpler and
-// more dynamic.
-// look up forIn and forEach ??
